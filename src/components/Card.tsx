f5e4489cd32c6edb4ab3765c57ae839d27f5bf10
yashtawar <yashtawar810@gmail.com>
2024-07-21 14:28:19 +0530

    added a card

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/card.css'
+
+function Card() {
+  return (
+    <div className='container'>
+        
+        <img src="https://img.freepik.com/free-photo/fresh-ripe-apple-white-background-close-up-generative-ai_188544-8731.jpg" alt="this is an image" />
+        
+  
+        <h1>This is heading for the product</h1>
+  
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi mollitia harum hic possimus architecto</p>
+
+        <button>Action</button>
+
+    </div>
+  )
+}
+
+export default Card
